41c01541b11aec9158f485d64634d81ea7429096
MeletChirino <melet.chirino@gmail.com>
2021-06-01 18:36:42 +0200

    script updated to install virtual environment as well

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ clear
 echo "==============="
 echo "Raspi as Acces Point"
 sudo apt update
-sudo apt install -y hostapd dnsmasq
+sudo apt install -y hostapd dnsmasq python3 python3-pip python3-venv libtiff5 libopenjp2-7 libatlas-base-dev
+
 sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
 sudo systemctl unmask hostapd.service
 sudo systectl enable hostapd.service
@@ -15,3 +16,13 @@ sudo netfilter-persistent save
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.BAK
 sudo cp dnsmasq.conf /etc/dnsmasq.conf
 sudo cp hostapd.conf /etc/hostapd/hostapd.conf
+echo "Raspberry Configured as Access Point, type something to continue"
+read x
+
+echo "==============="
+echo "Installing Virtual Environment"
+python3 -m venv ./venv
+source venv/bin/activate
+pip3 install -r requierements.txt
+echo "virtual environment installed type something to continue"
+read x
